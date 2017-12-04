d78ba7a2fd976c82eac07dee41bc8a2f6a798755
Shashi Thipparthy <shashi_thipparthy@gallup.com>
2017-12-03 21:59:05 -0600

    changes

--- my-app-v1/src/index.js
+++ my-app-v1/src/index.js
@@ -1,33 +1,59 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {UIRouter, UIView, UISref, UISrefActive, pushStateLocationPlugin} from 'ui-router-react';
+import { UIRouter, UIView, UISref, UISrefActive, pushStateLocationPlugin } from 'ui-router-react';
 import registerServiceWorker from './registerServiceWorker';
 
 var helloState = {
-  name: 'hello',
-  url: '/hello',
-  component: () => <h3>hello world</h3>
+    name: 'hello',
+    url: '/hello',
+    component: () => <h3>hello world</h3>
 }
 
 var aboutState = {
-  name: 'about',
-  url: '/about',
-  component: () => <h3>Its the UI-Router hello world app!</h3>
+    name: 'about',
+    url: '/about',
+    component: () => <h3>Its the UI-Router hello world app!</h3>
 }
 
 ReactDOM.render(
-  <UIRouter plugins={[pushStateLocationPlugin]} states={[helloState, aboutState]}>
-    <div>
-      <UISrefActive class="active">
-        <UISref to="hello"><a>Hello</a></UISref>
-      </UISrefActive>
-      <UISrefActive class="active">
-        <UISref to="about"><a>About</a></UISref>
-      </UISrefActive>
+    <UIRouter plugins={[pushStateLocationPlugin]} states={[helloState, aboutState]}>
+        <nav className="navbar navbar-toggleable-md navbar-light bg-faded">
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav mr-auto">
+                    <UISrefActive class="active" key="1">
+                        <UISref to="hello"><li className="nav-item active"><a href="javascript.void(0);"> Home </a> </li></UISref>
 
-      <UIView/>
-    </div>
-  </UIRouter>,
-  document.getElementById('root')
+                    </UISrefActive>
+
+                    <UISrefActive class="active" key="1">
+                        <UISref to="about"><li className="nav-item active"><a href="javascript.void(0);"> About </a> </li></UISref>
+
+                    </UISrefActive>
+                </ul>
+
+            </div>
+
+            {/* 
+            <ul>
+                <li>
+                    <UISrefActive className="active">
+                        <UISref to="hello"><a>Hello</a></UISref>
+                    </UISrefActive>
+
+                </li>
+                <li>
+                    <UISrefActive className="active">
+                        <UISref to="about"><a>About</a></UISref>
+                    </UISrefActive>
+
+                </li>
+            </ul> */}
+
+
+            <UIView />
+
+        </nav>
+    </UIRouter>,
+    document.getElementById('root')
 );
 registerServiceWorker();
